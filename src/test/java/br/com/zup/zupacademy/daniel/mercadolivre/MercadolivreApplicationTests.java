fe8691014dc41e3c9effd94abf0339755c842982
Daniel Ribeiro Santiago <daniel.santiago@zup.com.br>
2021-05-25 01:04:03 -0300

    commit com o setup inicial do projeto

---
+++ src/test/java/br/com/zup/zupacademy/daniel/mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.zupacademy.daniel.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
